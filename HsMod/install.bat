044da4feeedc701de462ec2603f1ef57356ae19d
Pik-4 <88079004+Pik-4@users.noreply.github.com>
2024-10-19 14:11:49 +0800

    rm -rf

--- HsMod/install.bat
+++
@@ -1,7 +0,0 @@
-cd "%~dp0"
-
-if exist "%PROGRAMFILES(x86)%\Hearthstone\BepInEx\core\BepInEx.dll" (
-    copy /y Release\HsMod.dll "%PROGRAMFILES(x86)%\Hearthstone\BepInEx\plugins\HsMod.dll"
-) else (
-    echo BepInEx is not exist!
-)
